c893e3a9cd6fc54ab6e4c62f81366cba239d845a
yossi-yuto <yoshimura.yuto@g.wakayama-u.jp>
2025-02-25 15:58:49 +0900

    revised betas and thresholds

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -9,6 +9,33 @@ def get_maxFscore_and_threshold(true_1d, pred_1d) -> tuple:
     fbetas = np.divide(numerator, denom, out=np.zeros_like(denom), where=(denom!=0))
     return np.max(fbetas), thres[np.argmax(fbetas)]
 
+def get_maxFscore_and_threshold_multi_beta(true_1d: np.ndarray, pred_1d: np.ndarray, betas: list=[0.3, 0.5, 1.0]) -> dict:
+    """
+    各 beta 値に対して、最大の F スコアと対応する閾値を計算して辞書で返す関数
+
+    :param true_1d: np.ndarray, shape=(n,)
+    :param pred_1d: np.ndarray, shape=(n,)
+    :param beta: list of float, F スコアの計算で使用する beta 値のリスト
+    :return: dict, 各 beta をキーとして、{"max_fscore": 最大の F スコア, "threshold": そのときの閾値} を値に持つ辞書
+    """
+    precisions, recalls, thres = precision_recall_curve(true_1d, pred_1d)
+    
+    # precision と recall は thres より 1 つ多いので、閾値と対応付けるために最後の要素を除外
+    precisions_cut = precisions[:-1]
+    recalls_cut = recalls[:-1]
+    
+    result :dict = {}
+    for _beta in betas:
+        numerator = (1 + _beta) * (precisions_cut * recalls_cut)
+        denom = (_beta * precisions_cut) + recalls_cut
+        fbetas = np.divide(numerator, denom, out=np.zeros_like(denom), where=(denom != 0))
+        max_index = np.argmax(fbetas)
+        max_fscore = fbetas[max_index]
+        corresponding_threshold = thres[max_index]
+        result[_beta] = {"max_fscore": max_fscore, "threshold": corresponding_threshold}
+    
+    return result
+
 
 def calculate_iou(ground_truth, prediction):
     """
@@ -26,7 +53,7 @@ def calculate_iou(ground_truth, prediction):
     return iou
 
 
-def calc_iou_multi_thresh(ground_truth: np.ndarray, prediction: np.ndarray) -> dict:
+def calc_iou_multi_thresh(ground_truth: np.ndarray, prediction: np.ndarray, thresholds :list = [0.3, 0.5, 0.7]) -> dict:
     """
     予測ラベルのしきい値を変化させながらIoUを計算する関数
     :param ground_truth: 正解ラベル (0と1で構成されたバイナリマスク)
@@ -36,7 +63,7 @@ def calc_iou_multi_thresh(ground_truth: np.ndarray, prediction: np.ndarray) -> d
     assert ground_truth.shape == prediction.shape, f"ground_truth.shape != prediction.shape"
     assert prediction.max() <= 1 and prediction.min() >= 0, f"prediction.max() <= 1 and prediction.min() >= 0"
     iou_dict = {}
-    for thres in np.arange(0.1, 1.0, 0.1):
+    for thres in thresholds:
         iou = calculate_iou(ground_truth, prediction > thres)
         iou_dict[thres] = iou
     return iou_dict
