70c8bac6eb3ef2f76ef86b984285a4810741e0da
yossi-yuto <yoshimura.yuto@g.wakayama-u.jp>
2025-03-17 01:45:31 +0900

    revised: changes from abs path to relative path

--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,4 @@
 # coding: utf-8
-VMD_training_root = ('/data2/yoshimura/mirror_detection/proj_mirror_video/VMD/train_filtered', 'video', 'VMD_train')
-VMD_valid_root = ('/data2/yoshimura/mirror_detection/proj_mirror_video/VMD/val_filtered', 'video', 'VMD_train')
-VMD_test_root = ('/data2/yoshimura/mirror_detection/proj_mirror_video/VMD/test', 'video', 'VMD_test')
+VMD_training_root = ('../VMD/train', 'video', 'VMD_train')
+VMD_valid_root = ('../VMD/val', 'video', 'VMD_train')
+VMD_test_root = ('../VMD/test', 'video', 'VMD_test')
